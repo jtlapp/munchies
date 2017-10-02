d7be3ba35856e83ee6dbcd8fdff40169a4770f8e
Joe Lapp <arachnojoe@gmail.com>
2017-10-02 17:12:35 -0500

    Made builds start clean

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,12 @@ const gulp = require('gulp');
 const shell = require('gulp-shell');
 const del = require('del');
 
-gulp.task("build",
+gulp.task("build", [
+    'clean',
+    'update'
+]);
+
+gulp.task("update",
     shell.task('tsc', { 
         cwd: __dirname
     })
@@ -18,7 +23,7 @@ gulp.task("watch", () => {
     gulp.watch([
         'src/**/*.ts',
         'demo/**/*.ts'
-    ], [ "build" ]);
+    ], [ "update" ]);
 });
 
 gulp.task("default", ["watch"]);
